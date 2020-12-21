998244002bdd9178feb5e0da64a24904293a3682
Javiera Briones <Javiera.Briones.16@hotmail.com>
2020-12-21 17:25:23 -0300

    Separate views persona/proyecto

--- gestionproyecto/__manifest__.py
+++ gestionproyecto/__manifest__.py
@@ -25,6 +25,7 @@
     'data': [
       'security/ir.model.access.csv',
         'views/view_proyecto.xml',
+        'views/view_persona.xml'
         #'views/templates.xml',
     ],
     # only loaded in demonstration mode
